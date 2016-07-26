1d9a3478fa02d7320397feb06c637e147056253f
Kaleb Elwert <kelwert@mtu.edu>
2016-07-26 01:42:54 -0700

    Add Client as a wrapper around Conn (#30)
    * Add Client as a wrapper around Conn
    * Install testify using go test -i
    * Make gocyclo more forgiving for now

---
+++ client.go
@@ -0,0 +1,80 @@
+package irc
+
+import "io"
+
+// ClientConfig is a structure used to configure a Client.
+type ClientConfig struct {
+	// General connection information.
+	Nick string
+	Pass string
+	User string
+	Name string
+
+	// Handler is used for message dispatching.
+	Handler Handler
+}
+
+// Client is a wrapper around Conn which is designed to make common operations
+// much simpler.
+type Client struct {
+	*Conn
+	config ClientConfig
+
+	// Internal state
+	currentNick string
+}
+
+// NewClient creates a client given an io stream and a client config.
+func NewClient(rwc io.ReadWriteCloser, config ClientConfig) *Client {
+	return &Client{
+		Conn:   NewConn(rwc),
+		config: config,
+	}
+}
+
+// Run starts the main loop for this IRC connection. Note that it may break in
+// strange and unexpected ways if it is called again before the first connection
+// exits.
+func (c *Client) Run() error {
+	c.currentNick = c.config.Nick
+
+	if c.config.Pass != "" {
+		c.Writef("PASS :%s", c.config.Pass)
+	}
+
+	c.Writef("NICK :%s", c.config.Nick)
+	c.Writef("USER %s 0.0.0.0 0.0.0.0 :%s", c.config.User, c.config.Name)
+
+	for {
+		m, err := c.ReadMessage()
+		if err != nil {
+			return err
+		}
+
+		switch m.Command {
+		case "PING":
+			reply := m.Copy()
+			reply.Command = "PONG"
+			c.WriteMessage(reply)
+		case "NICK":
+			if m.Prefix.Name == c.currentNick && len(m.Params) > 0 {
+				c.currentNick = m.Params[0]
+			}
+		case "001":
+			c.currentNick = m.Params[0]
+		case "433", "437":
+			c.currentNick = c.currentNick + "_"
+			c.Writef("NICK :%s", c.currentNick)
+		}
+
+		if c.config.Handler != nil {
+			c.config.Handler.Handle(c, m)
+		}
+	}
+}
+
+// CurrentNick returns what the nick of the client is known to be at this point
+// in time.
+func (c *Client) CurrentNick() string {
+	return c.currentNick
+}
---
+++ client_test.go
@@ -0,0 +1,114 @@
+package irc
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestHandler struct {
+	messages []*Message
+}
+
+func (th *TestHandler) Handle(c *Client, m *Message) {
+	th.messages = append(th.messages, m)
+}
+
+func (th *TestHandler) Messages() []*Message {
+	ret := th.messages
+	th.messages = nil
+	return ret
+}
+
+func TestClient(t *testing.T) {
+	rwc := newTestReadWriteCloser()
+	config := ClientConfig{
+		Nick: "test_nick",
+		Pass: "test_pass",
+		User: "test_user",
+		Name: "test_name",
+	}
+	c := NewClient(rwc, config)
+	err := c.Run()
+	assert.Equal(t, io.EOF, err)
+
+	testLines(t, rwc, []string{
+		"PASS :test_pass",
+		"NICK :test_nick",
+		"USER test_user 0.0.0.0 0.0.0.0 :test_name",
+	})
+
+	rwc.server.WriteString("PING :hello world\r\n")
+	err = c.Run()
+	assert.Equal(t, io.EOF, err)
+	testLines(t, rwc, []string{
+		"PASS :test_pass",
+		"NICK :test_nick",
+		"USER test_user 0.0.0.0 0.0.0.0 :test_name",
+		"PONG :hello world",
+	})
+
+	rwc.server.WriteString(":test_nick NICK :new_test_nick\r\n")
+	err = c.Run()
+	assert.Equal(t, io.EOF, err)
+	testLines(t, rwc, []string{
+		"PASS :test_pass",
+		"NICK :test_nick",
+		"USER test_user 0.0.0.0 0.0.0.0 :test_name",
+	})
+	assert.Equal(t, "new_test_nick", c.CurrentNick())
+
+	rwc.server.WriteString("001 :new_test_nick\r\n")
+	err = c.Run()
+	assert.Equal(t, io.EOF, err)
+	testLines(t, rwc, []string{
+		"PASS :test_pass",
+		"NICK :test_nick",
+		"USER test_user 0.0.0.0 0.0.0.0 :test_name",
+	})
+	assert.Equal(t, "new_test_nick", c.CurrentNick())
+
+	rwc.server.WriteString("433\r\n")
+	err = c.Run()
+	assert.Equal(t, io.EOF, err)
+	testLines(t, rwc, []string{
+		"PASS :test_pass",
+		"NICK :test_nick",
+		"USER test_user 0.0.0.0 0.0.0.0 :test_name",
+		"NICK :test_nick_",
+	})
+	assert.Equal(t, "test_nick_", c.CurrentNick())
+}
+
+func TestClientHandler(t *testing.T) {
+	handler := &TestHandler{}
+	rwc := newTestReadWriteCloser()
+	config := ClientConfig{
+		Nick: "test_nick",
+		Pass: "test_pass",
+		User: "test_user",
+		Name: "test_name",
+
+		Handler: handler,
+	}
+
+	rwc.server.WriteString("001 :hello_world\r\n")
+	c := NewClient(rwc, config)
+	err := c.Run()
+	assert.Equal(t, io.EOF, err)
+
+	testLines(t, rwc, []string{
+		"PASS :test_pass",
+		"NICK :test_nick",
+		"USER test_user 0.0.0.0 0.0.0.0 :test_name",
+	})
+
+	assert.EqualValues(t, []*Message{
+		&Message{
+			Prefix:  &Prefix{},
+			Command: "001",
+			Params:  []string{"hello_world"},
+		},
+	}, handler.Messages())
+}
